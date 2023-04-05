8114320c3091c7d48acecdce2465f6c62a60f295
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-05 18:16:16 -0400

    feat: class for spaceMan

--- app.js
+++ app.js
@@ -15,9 +15,30 @@ window.addEventListener('DOMContentLoaded', function () {
 // ====================== SETUP FOR CANVAS RENDERING ======================= //
 
 // ====================== ENTITIES ======================= //
+class spaceMan {
+    constructor(x, y, color, width, height) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.height = height;
+        this.width = width;
+        this.alive = true;
 
+        this.render = function () {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+}
 // ====================== HELPER FUNCTIONS ======================= //
 
 // ====================== GAME PROCESSES ======================= //
+//Im creating a game loop to keep the hoard coming continuously
 
+function gameLoop() {
+    //this will be rectangle for now, but plan to change img
+    ctx.clearRect(0, 0, game.clientWidth, game.height);
+
+
+}
 // ====================== COLLISION DETECTION ======================= //
