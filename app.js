bf1bd6a0ccd70761cd726467e3f42e87ab0140b5
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-05 18:10:23 -0400

    add: h1 to html, and global var to app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// GLOBAL DOM / VARIABLES
+let astronaut;
+let alien1;
+let alien2;
+let alien3;
+let alien4;
+const game = document.getElementById('game');
+const ctx = game.getContext('2d');
+// ====================== PAINT INTIAL SCREEN ======================= //
+
+// EVENT LISTENERS
+window.addEventListener('DOMContentLoaded', function () {
+
+})
+// ====================== SETUP FOR CANVAS RENDERING ======================= //
+
+// ====================== ENTITIES ======================= //
+
+// ====================== HELPER FUNCTIONS ======================= //
+
+// ====================== GAME PROCESSES ======================= //
+
+// ====================== COLLISION DETECTION ======================= //
