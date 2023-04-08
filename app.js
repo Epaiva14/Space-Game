d659ffad75d7ff3ac266f5490cf05c7406ed55ab
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-04-08 17:32:56 -0400

    fix: fixed an error where the 4th alien wouldnt register the detectHit

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ const ctx = game.getContext('2d');
 // loads all characters in the game
 window.addEventListener('DOMContentLoaded', function () {
     astronaut = new Spaceman(350, 525);
-    alien1 = new Alien(0, 0, alienImg1, 50, 50);
-    alien2 = new Alien(200, 0, alienImg2, 50, 50);
-    alien3 = new Alien(500, 0, alienImg3, 50, 50);
-    alien4 = new Alien(700, 0, alienImg4, 50, 50);
+    alien1 = new Alien(0, 0, alienImg1);
+    alien2 = new Alien(200, 0, alienImg2);
+    alien3 = new Alien(400, 0, alienImg3);
+    alien4 = new Alien(600, 0, alienImg4);
     plasmaWeapon = new Weapon(astronaut.x, astronaut.y, plasma, 50, 50);
     const runGame = this.setInterval(gameLoop, 60);
 })
@@ -55,19 +55,21 @@ class Spaceman {
 }
 
 class Alien {
-    constructor(x, y, image, width, height) {
+    constructor(x, y, image) {
         this.x = x;
         this.y = y;
         this.image = image;
-        this.height = height;
-        this.width = width;
+        this.height = 50;
+        this.width = 50;
         this.alive = true;
 
 
         this.render = function () {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         }
+
     }
+
 }
 // still trying to figure this out
 class Weapon {
@@ -97,13 +99,13 @@ function spaceWalk(e) {
     // console.log('walking :', e.key);
 
     if (e.key === 'Arrow Left' || e.key === 'a') {
-        astronaut.x - 10 >= 0 ? (astronaut.x -= 10) : null;
+        astronaut.x - 30 >= 0 ? (astronaut.x -= 30) : null;
     } else if (e.key === 'Arrow Right' || e.key === 'd') {
-        astronaut.x + 10 <= game.width - astronaut.width ? (astronaut.x += 10) : null;
+        astronaut.x + 30 <= game.width - astronaut.width ? (astronaut.x += 30) : null;
     } else if (e.key === 'Arrow Up' || e.key === 'w') {
-        astronaut.y - 10 >= 0 ? (astronaut.y -= 10) : null;
+        astronaut.y - 30 >= 0 ? (astronaut.y -= 30) : null;
     } else if (e.key === 'Arrow Down' || e.key === 's') {
-        astronaut.y + 10 <= game.height - astronaut.height ? (astronaut.y += 10) : null;
+        astronaut.y + 30 <= game.height - astronaut.height ? (astronaut.y += 30) : null;
     }
     if (e.key === 'b') {
         console.log('shoot');
@@ -133,6 +135,7 @@ function alienWalk() {
 
 
 // ====================== HELPER FUNCTIONS ======================= //
+const newAliens = [];
 function spawnAlien1() {
     alien1.alive = false;
 
@@ -147,19 +150,40 @@ function spawnAlien1() {
 }
 
 function spawnAlien2() {
-    if (alien2.not.alive) {
-        alien2.render();
-    }
+    alien2.alive = false;
+
+    setTimeout(function () {
+        let randomX = Math.floor(Math.random() * (game.width - 50));
+        let zeroY = game.height - 600;
+        const alienType = [alienImg1, alienImg2, alienImg3, alienImg4];
+        let randomIndex = Math.floor(Math.random() * (alienType.length - 1));
+        let randomAlien = alienType[randomIndex];
+        alien2 = new Alien(randomX, zeroY, randomAlien, 50, 50)
+    }, 1000);
 }
 function spawnAlien3() {
-    if (alien3.not.alive) {
-        alien3.render();
-    }
+    alien3.alive = false;
+
+    setTimeout(function () {
+        let randomX = Math.floor(Math.random() * (game.width - 50));
+        let zeroY = game.height - 600;
+        const alienType = [alienImg1, alienImg2, alienImg3, alienImg4];
+        let randomIndex = Math.floor(Math.random() * (alienType.length - 1));
+        let randomAlien = alienType[randomIndex];
+        alien3 = new Alien(randomX, zeroY, randomAlien, 50, 50)
+    }, 1000);
 }
 function spawnAlien4() {
-    if (alien4.not.alive) {
-        alien4.render();
-    }
+    alien4.alive = false;
+
+    setTimeout(function () {
+        let randomX = Math.floor(Math.random() * (game.width - 50));
+        let zeroY = game.height - 600;
+        const alienType = [alienImg1, alienImg2, alienImg3, alienImg4];
+        let randomIndex = Math.floor(Math.random() * (alienType.length - 1));
+        let randomAlien = alienType[randomIndex];
+        alien4 = new Alien(randomX, zeroY, randomAlien, 50, 50)
+    }, 1000);
 }
 
 
@@ -175,6 +199,18 @@ function gameLoop() {
     if (alien1.alive) {
         alien1.render();
         let hit = detectHit(astronaut, alien1);
+    }
+    if (alien2.alive) {
+        alien2.render();
+        let hit = detectHit2(astronaut, alien2);
+    }
+    if (alien3.alive) {
+        alien3.render();
+        let hit = detectHit3(astronaut, alien3);
+    }
+    if (alien4.alive) {
+        alien4.render();
+        let hit = detectHit4(astronaut, alien4);
     }
     astronaut.render();
     // spaceWalk();
@@ -198,8 +234,67 @@ function detectHit(player, opponent) {
         let newScore = Number(score.textContent) + 1;
         score.textContent = newScore;
 
-        return spawnAlien1();
+        return spawnAlien1()
     } else {
         return false;
     }
+
+
+}
+function detectHit2(player, opponent) {
+    let hitTest = (player.y + player.height > opponent.y &&
+        player.y < opponent.y + opponent.height &&
+        player.x + player.width > opponent.x &&
+        player.x < opponent.x + opponent.width
+    );
+
+    if (hitTest) {
+        // add 1 point to current score
+        let newScore = Number(score.textContent) + 1;
+        score.textContent = newScore;
+
+        return spawnAlien2()
+    } else {
+        return false;
+    }
+
+
+}
+function detectHit3(player, opponent) {
+    let hitTest = (player.y + player.height > opponent.y &&
+        player.y < opponent.y + opponent.height &&
+        player.x + player.width > opponent.x &&
+        player.x < opponent.x + opponent.width
+    );
+
+    if (hitTest) {
+        // add 1 point to current score
+        let newScore = Number(score.textContent) + 1;
+        score.textContent = newScore;
+
+        return spawnAlien3()
+    } else {
+        return false;
+    }
+
+
+}
+function detectHit4(player, opponent) {
+    let hitTest = (player.y + player.height > opponent.y &&
+        player.y < opponent.y + opponent.height &&
+        player.x + player.width > opponent.x &&
+        player.x < opponent.x + opponent.width
+    );
+
+    if (hitTest) {
+        // add 1 point to current score
+        let newScore = Number(score.textContent) + 1;
+        score.textContent = newScore;
+
+        return spawnAlien4()
+    } else {
+        return false;
+    }
+
+
 }
